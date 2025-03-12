2214c40309f47a23de948e3ba3c87743814b7a39
Your Name <you@example.com>
2025-03-12 16:08:48 +0300

    Add support for 12GB RAM phone edition

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 This repo contians **Base Guide for installation/upgrading** and **Scripts for automatic building of Ubuntu RootFS, Mainline Kernel, Firmware package, ALSA configs** for OnePlus 12R/Ace 3
 
 # Where do i get needed files?
-Just go to the "Actions" tab, open most recent build and download files named **rootfs_(Desktop Environment)_(Kernel version)** and **boot-oneplus-aston_(Kernel version).img**
+Just go to the "Actions" tab, open most recent build and download files named **rootfs_(Desktop Environment)_(Kernel version)** and **boot-oneplus-aston_(Kernel version)_(Phone RAM Size).img**
 <br>For upgrading - instead, download all files available, **except for rootfs**
 
 ## Upgrading steps (From running Ubuntu)
--- aston-kernel_build.sh
+++ aston-kernel_build.sh
@@ -9,7 +9,12 @@ sed -i "s/Version:.*/Version: ${_kernel_version}/" $1/linux-oneplus-aston/DEBIAN
 cat $1/linux/arch/arm64/boot/Image $1/linux/arch/arm64/boot/dts/qcom/sm8550-oneplus-aston.dtb > $1/linux/Image_w_dtb
 gzip Image_w_dtb
 chmod +x $1/mkbootimg
-$1/mkbootimg --header_version 4 --base 0x0 --kernel $1/linux/Image_w_dtb.gz -o $1/boot.img
+$1/mkbootimg --header_version 4 --base 0x0 --os_version 15.0.0 --os_patch_level 2025-02 --kernel $1/linux/Image_w_dtb.gz -o $1/boot16G.img
+
+cat $1/linux/arch/arm64/boot/Image $1/linux/arch/arm64/boot/dts/qcom/sm8550-oneplus-aston_12G.dtb > $1/linux/Image_w_dtb
+gzip Image_w_dtb
+chmod +x $1/mkbootimg
+$1/mkbootimg --header_version 4 --base 0x0 --os_version 15.0.0 --os_patch_level 2025-02 --kernel $1/linux/Image_w_dtb.gz -o $1/boot12G.img
 
 rm $1/linux-oneplus-aston/usr/dummy
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=$1/linux-oneplus-aston/usr modules_install
