6e08b7ad83b959e87f2020d399dbbdf6f6d68806
Shandorman <98683030+jiganomegsdfdf@users.noreply.github.com>
2025-03-22 13:16:45 +0300

    Add back support for ColorOS14, 16GB

--- aston-kernel_build.sh
+++ aston-kernel_build.sh
@@ -6,14 +6,18 @@ make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
 _kernel_version="$(make kernelrelease -s)"
 sed -i "s/Version:.*/Version: ${_kernel_version}/" $1/linux-oneplus-aston/DEBIAN/control
 
+chmod +x $1/mkbootimg
+
 cat $1/linux/arch/arm64/boot/Image $1/linux/arch/arm64/boot/dts/qcom/sm8550-oneplus-aston.dtb > $1/linux/Image_w_dtb
 gzip Image_w_dtb
-chmod +x $1/mkbootimg
 $1/mkbootimg --header_version 4 --base 0x0 --os_version 15.0.0 --os_patch_level 2025-02 --kernel $1/linux/Image_w_dtb.gz -o $1/boot16G.img
+
+cat $1/linux/arch/arm64/boot/Image $1/linux/arch/arm64/boot/dts/qcom/sm8550-oneplus-aston_16G_A14.dtb > $1/linux/Image_w_dtb
+gzip Image_w_dtb
+$1/mkbootimg --header_version 4 --base 0x0 --os_version 15.0.0 --os_patch_level 2025-02 --kernel $1/linux/Image_w_dtb.gz -o $1/boot16G_A14.img
 
 cat $1/linux/arch/arm64/boot/Image $1/linux/arch/arm64/boot/dts/qcom/sm8550-oneplus-aston_12G.dtb > $1/linux/Image_w_dtb
 gzip Image_w_dtb
-chmod +x $1/mkbootimg
 $1/mkbootimg --header_version 4 --base 0x0 --os_version 15.0.0 --os_patch_level 2025-02 --kernel $1/linux/Image_w_dtb.gz -o $1/boot12G.img
 
 rm $1/linux-oneplus-aston/usr/dummy
